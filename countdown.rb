b82c9d1ef26f09a3e622a202d211b0680cd16bb9
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-06 16:05:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,9 @@ def countdown (number)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-end
   
+  if number == 0 
+    puts "HAPPY NEW YEAR!"
+  end
 end
-puts "HAPPY NEW YEAR!"
+end
